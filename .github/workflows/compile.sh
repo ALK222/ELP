77a8c152d1137c05fa63b9812af35b77116f3403
A.L.K <44205191+ALK222@users.noreply.github.com>
2023-02-01 11:55:58 +0100

    compile action

---
+++ .github/workflows/compile.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd ../../Apuntes
+
+xelatex -file-line-error -interaction=nonstopmode Apuntes-ELP.tex
+
+makeglossaries Apuntes-ELP.tex
+
+xelatex -file-line-error -interaction=nonstopmode Apuntes-ELP.tex
+
+xelatex -file-line-error -interaction=nonstopmode Apuntes-ELP.tex
